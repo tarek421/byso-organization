173f0078762919f392207e61cafd0658db6352a6
tarek421 <tarekmahmud740@gmail.com>
2023-05-12 11:39:12 +0600

    something change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/team/:id" element={<PageNotFound />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="*" exact="true" element={<PageNotFound />} />
       </Routes>
     </div>
   );
